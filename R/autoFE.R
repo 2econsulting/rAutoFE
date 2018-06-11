110f34d34c1b8af17f879e8005a4a2bed4d11fa3
jacobgreen1984 <jacobgreen@github.com>
2018-06-11 18:10:42 +0900

    jacob

--- R/autoFE.R
+++ R/autoFE.R
@@ -3,7 +3,7 @@
 #' @param train data.table object
 #' @param valid data.table object
 #' @param test data.table object
-#' @param savePath save path 
+#' @param savePath save path
 #' @examples
 #' library(data.table)
 #' library(rAutoFE)
@@ -24,11 +24,11 @@
 #' dataset <- autoFE(train=train, valid=valid, test=test, x=x, y=y, savePath=savePath, verbose=TRUE)
 #' @export
 autoFE <- function(train, valid, test, x, y, savePath, verbose=FALSE){
-  
+
   if(verbose==FALSE) h2o.no_progress()
-  
+
   cat(">> [PART1] auto feature engineering start! \n")
-  
+
   # 1.1 reduceDimCat
   vars_w_highlevels <- names(which(sapply(train, nlevels)>30))
   fit_reduceDimCat <- rAutoFE::reduceDimCat_fit(data=train, column_name=vars_w_highlevels, min_percentage=0.01, max_numOflevel=30)
@@ -37,7 +37,7 @@ autoFE <- function(train, valid, test, x, y, savePath, verbose=FALSE){
   test  <- rAutoFE::reduceDimCat_transform(data = test,  fit = fit_reduceDimCat)
   saveRDS(fit_reduceDimCat, file.path(savePath, "fit_reduceDimCat.rda"))
   cat(">> 1.1 reduceDimCat done! \n")
-  
+
   # 1.2 frequencyEncoding
   vars_w_multilevels <- names(which(sapply(train, nlevels)>2))
   fit_frequencyEncoding <- frequencyEncoding_fit(dt = train, x = vars_w_multilevels)
@@ -46,7 +46,7 @@ autoFE <- function(train, valid, test, x, y, savePath, verbose=FALSE){
   test  <- frequencyEncoding_transform(dt = test, fit = fit_frequencyEncoding)
   saveRDS(fit_frequencyEncoding, file.path(savePath, "fit_frequencyEncoding.rda"))
   cat(">> 1.2 frequencyEncoding done! \n")
-  
+
   # 1.3 targetEncoding
   vars_w_multilevels <- names(which(sapply(train, nlevels)>2))
   fit_targetEncoding <- targetEncoding_fit(dt = train, x = vars_w_multilevels, y = y)
@@ -55,9 +55,9 @@ autoFE <- function(train, valid, test, x, y, savePath, verbose=FALSE){
   test  <- targetEncoding_transform(dt = test, fit = fit_targetEncoding)
   saveRDS(fit_targetEncoding, file.path(savePath, "fit_targetEncoding.rda"))
   cat(">> 1.3 targetEncoding done! \n")
-  
+
   cat(">> [PART2] auto feature engineering with variable importance start! \n")
-  
+
   # 2.1 extract vi
   train_hex <- as.h2o(train)
   valid_hex <- as.h2o(valid)
@@ -77,15 +77,15 @@ autoFE <- function(train, valid, test, x, y, savePath, verbose=FALSE){
   saveRDS(baseline_ml, file.path(savePath, "baseline_ml.rda"))
   saveRDS(baseline_vi, file.path(savePath, "baseline_vi.rda"))
   cat(">> 2.1 extract vi done! \n")
-  
-  # 2.2 bestNormalize for vi 
+
+  # 2.2 bestNormalize for vi
   fit_bestNormalize <- rAutoFE::bestNormalize_fit(dt=train, vi=baseline_vi$top_vi)
   train <- rAutoFE::bestNormalize_transform(dt=train, fit=fit_bestNormalize)
   valid <- rAutoFE::bestNormalize_transform(dt=valid, fit=fit_bestNormalize)
   test  <- rAutoFE::bestNormalize_transform(dt=test, fit=fit_bestNormalize)
   saveRDS(fit_bestNormalize, file.path(savePath, "fit_bestNormalize.rda"))
   cat(">> 2.2 bestNormalize for vi done! \n")
-  
+
   # 2.3 bin4numeric for vi
   fit_bin4numeric <- bin4numeric_fit(dt=train, vi=baseline_vi$top_vi)
   train <- bin4numeric_transform(dt=train, fit=fit_bin4numeric)
@@ -93,22 +93,22 @@ autoFE <- function(train, valid, test, x, y, savePath, verbose=FALSE){
   test <- bin4numeric_transform(dt=test, fit=fit_bin4numeric)
   saveRDS(fit_bin4numeric, file.path(savePath, "fit_bin4numeric.rda"))
   cat(">> 2.3 bin4numeric for vi done! \n")
-  
-  # 2.4 featureCombination for vi 
+
+  # 2.4 featureCombination for vi
   train <- featureComb_fit_transform(dt=train, vi=baseline_vi$top_vi)
   valid <- featureComb_fit_transform(dt=valid, vi=baseline_vi$top_vi)
   test <- featureComb_fit_transform(dt=test, vi=baseline_vi$top_vi)
   cat(">> 2.4 featureCombination for vi done! \n")
-  
-  # 2.5 weightOfEvidence for vi 
+
+  # 2.5 weightOfEvidence for vi
   fit_woe <- WoE_fit(data=train, target.var=y, pred.var=baseline_vi$top_vi)
   train <- WoE_transform(data = train, fit = fit_woe)
   valid <- WoE_transform(data = valid, fit = fit_woe)
   test <- WoE_transform(data = test, fit = fit_woe)
   saveRDS(fit_woe, file.path(savePath, "fit_woe.rda"))
   cat(">> 2.5 weightOfEvidence for vi done! \n")
-  
-  # reduce high-levels for final step  
+
+  # reduce high-levels for final step
   vars_w_highlevels2 <- names(which(sapply(train, nlevels)>30))
   fit_reduceDimCat2 <- rAutoFE::reduceDimCat_fit(data=train, column_name=vars_w_highlevels2, min_percentage=0.01, max_numOflevel=30)
   train <- rAutoFE::reduceDimCat_transform(data = train, fit = fit_reduceDimCat2)
@@ -116,32 +116,24 @@ autoFE <- function(train, valid, test, x, y, savePath, verbose=FALSE){
   test  <- rAutoFE::reduceDimCat_transform(data = test,  fit = fit_reduceDimCat2)
   saveRDS(fit_reduceDimCat2, file.path(savePath, "fit_reduceDimCat2.rda"))
   cat(">> reduce high-levels for final step done! \n")
-  
-  # convert all char to factor 
+
+  # convert all char to factor
   changeCols = names(which(sapply(train, is.character)))
   train <- train[, (changeCols):=lapply(.SD, as.factor), .SDcols=changeCols]
   valid <- valid[, (changeCols):=lapply(.SD, as.factor), .SDcols=changeCols]
   test  <- test[, (changeCols):=lapply(.SD, as.factor), .SDcols=changeCols]
-  
+
   # remove one factor level
   fit_removeOneLevel <- removeOneLevel_fit(dt=train, nLevels=1)
-  train <- reduceDimCat_transform(data = train, fit = fit_removeOneLevel)
-  valid <- reduceDimCat_transform(data = valid, fit = fit_removeOneLevel)
-  test  <- reduceDimCat_transform(data = test,  fit = fit_removeOneLevel)
+  train <- removeOneLevel_transform(data = train, fit = fit_removeOneLevel)
+  valid <- removeOneLevel_transform(data = valid, fit = fit_removeOneLevel)
+  test  <- removeOneLevel_transform(data = test,  fit = fit_removeOneLevel)
   saveRDS(fit_removeOneLevel, file.path(savePath, "fit_removeOneLevel.rda"))
-  
+
   # output
-  output <- list(train=train, valid=valid, test=test) 
+  output <- list(train=train, valid=valid, test=test)
   return(output)
 }
-
-
-
-
-
-
-
-
 
 
 
